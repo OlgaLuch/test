22e81d6855331d2a1ae97045099b3e9fa53c77d6
OlgaLuch <olga.luchaninova@gmail.com>
2015-12-27 17:33:14 +0200

    delete coment

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Îëÿ
+ * User: ÃŽÃ«Ã¿
  * Date: 27.12.2015
  * Time: 17:06
- sadfsadfsadfsadf
- asdfsafsadfasdfasdfasf
  */
 if (1 < 2) {
     echo "Hello";
